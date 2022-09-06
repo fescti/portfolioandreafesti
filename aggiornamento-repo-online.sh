faea81e5e5ffbdc6a81da9feb46af2e5c125827c
fescti <festi.andrea@gmail.com>
2022-09-06 21:44:27 +0200

    modifiche

---
+++ aggiornamento-repo-online.sh
@@ -0,0 +1,11 @@
+# Make executable with chmod +x <<filename.sh>>
+
+cd  /c/Hugo/portfolioandreafesti/themes/apsho
+git add .
+git commit -m "modifiche"
+git push --all
+
+cd  /c/Hugo/portfolioandreafesti
+git add .
+git commit -m "modifiche"
+git push --all
