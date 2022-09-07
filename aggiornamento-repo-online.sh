0a520ca3b2c7d1168806e73ea2886aea2ac67bf4
fescti <festi.andrea@gmail.com>
2022-09-07 08:33:18 +0200

    modifiche

--- aggiornamento-repo-online.sh
+++ aggiornamento-repo-online.sh
@@ -1,5 +1,6 @@
-# Make executable with chmod +x <<filename.sh>>
-# Per avviare lo scripit posizionarsi nella cartella /c/Hugo/portfolioandreafesti e scrivere nel terminale bash aggiornamento-repo-online.sh
+###### Make executable with chmod +x <<filename.sh>>
+###### Per avviare lo scripit posizionarsi nella cartella /c/Hugo/portfolioandreafesti e scrivere nel terminale
+###### bash aggiornamento-repo-online.sh
 
 cd  /c/Hugo/portfolioandreafesti/themes/apsho
 git add .
